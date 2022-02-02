665b1346a36fbe2a6fedc0b723506fd5e6b6cead
adam <shah24@uw.edu>
2022-02-02 11:34:04 -0800

    saving

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import "./App.css";
 import TextInput from "./InputText";
 import { useState } from "react";
 import Message from "./Messages";
+//import { use100vh } from "react-div-100vh";
+//import { FiSend, FiCamera } from 'react-icons/fi'
 
 function App() {
+  //const height = use100vh();
+
   const [messages, setText] = useState([]);
   function sendMessage(text) {
     const newMessage = {
@@ -13,7 +17,7 @@ function App() {
     setText([newMessage, ...messages]);
   }
   return (
-    <div className="App">
+    <div className="App"  /*style={{ height: height, minHeight: height, maxHeight: height }} */>
       <header className="header">
         <div className="logo" />
         <span className="title">chatbotty!</span>
